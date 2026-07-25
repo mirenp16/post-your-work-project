4e0b657766f228e2b99859302896163010ace11e
mirenp16 <patelmiren1698@gmail.com>
2026-07-25 18:30:53 -0400

    refactor: Added a helper function to the bikeshare_starter.py file
    Added a helper print_separator function which can be used in get_filters() function in bikeshare_starter.py file.

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -6,6 +6,9 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+def print_separator():
+    print('-' * 40)
+
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
@@ -25,7 +28,7 @@ def get_filters():
     # get user input for day of week (all, monday, tuesday, ... sunday)
 
 
-    print('-'*40)
+    print_separator()
     return city, month, day
 
 
